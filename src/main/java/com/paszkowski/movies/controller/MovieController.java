45fcc866cb56c120e52c6027b72a463e673f027a
patryk <patrykoloss@gmail.com>
2023-04-14 16:19:36 +0200

    Add pagination, sorting and filtering

--- src/main/java/com/paszkowski/movies/controller/MovieController.java
+++ src/main/java/com/paszkowski/movies/controller/MovieController.java
@@ -7,12 +7,14 @@ import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import io.swagger.annotations.ApiParam;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
 import java.util.List;
-
 @RestController
 @RequestMapping("/movies")
 @Api(value = "Movie management system")
@@ -20,22 +22,59 @@ import java.util.List;
 public class MovieController {
 
     private final MovieService movieService;
+    @Autowired
+    private InsertMovies insertMovies;
 
     public MovieController(MovieService movieService) {
         this.movieService = movieService;
     }
-    @Autowired
-    private InsertMovies insertMovies;
+
+    @GetMapping
+    @ApiOperation(value = "Get all movies", response = Page.class)
+    public ResponseEntity<Page<Movie>> getAllMovies(
+            @RequestParam(defaultValue = "0") int page,
+            @RequestParam(defaultValue = "10") int size,
+            @RequestParam(defaultValue = "title") String sort,
+            @RequestParam(defaultValue = "asc") String direction
+    ) {
+        PageRequest pageRequest = PageRequest.of(
+                page, size,
+                direction.equalsIgnoreCase("asc") ? Sort.Direction.ASC : Sort.Direction.DESC,
+                sort
+        );
+        return ResponseEntity.ok(movieService.getAllMovies(pageRequest));
+    }
+
+    @GetMapping("/categories/{category}")
+    @ApiOperation(value = "Filter movies by category", response = Page.class)
+    public ResponseEntity<Page<Movie>> filterMoviesByCategory(
+            @PathVariable @ApiParam(value = "Category of the movie", required = true) String category,
+            @RequestParam(defaultValue = "0") int page,
+            @RequestParam(defaultValue = "10") int size,
+            @RequestParam(defaultValue = "title") String sort,
+            @RequestParam(defaultValue = "asc") String direction
+    ) {
+        PageRequest pageRequest = PageRequest.of(
+                page, size,
+                direction.equalsIgnoreCase("asc") ? Sort.Direction.ASC : Sort.Direction.DESC,
+                sort
+        );
+        return ResponseEntity.ok(movieService.filterMoviesByCategory(category, pageRequest));
+    }
 
     @PostMapping("/populate")
-    public String populateMovies() {
+    @ApiOperation(value = "Populate movies")
+    public ResponseEntity<String> populateMovies() {
         insertMovies.populate();
-        return "Movies have been populated!";
+        return ResponseEntity.ok("Movies have been populated!");
     }
-    @GetMapping
-    @ApiOperation(value = "Get all movies", response = List.class)
-    public ResponseEntity<List<Movie>> getAllMovies() {
-        return ResponseEntity.ok(movieService.getAllMovies());
+
+    @GetMapping("/{id}")
+    @ApiOperation(value = "Get a movie by id", response = Movie.class)
+    public ResponseEntity<Movie> getMovieById(
+            @PathVariable @ApiParam(value = "Id of the movie", required = true) Long id
+    ) {
+        return ResponseEntity.ok(movieService.getMovieById(id));
     }
 
     @PostMapping
@@ -46,34 +85,36 @@ public class MovieController {
 
     @PutMapping("/{id}")
     @ApiOperation(value = "Update a movie", response = Movie.class)
-    public ResponseEntity<Movie> updateMovie(@PathVariable @ApiParam(value = "Movie Id to update movie object", required = true) Long id,
-                                             @Valid @RequestBody Movie movie) {
+    public ResponseEntity<Movie> updateMovie(
+            @PathVariable @ApiParam(value = "Movie Id to update movie object", required = true) Long id,
+            @Valid @RequestBody Movie movie
+    ) {
         return ResponseEntity.ok(movieService.editMovie(id, movie));
     }
 
     @DeleteMapping("/{id}")
     @ApiOperation(value = "Delete a movie")
-    public ResponseEntity<Void> deleteMovie(@PathVariable @ApiParam(value = "Movie Id to delete movie object", required = true) Long id) {
+    public ResponseEntity<Void> deleteMovie(
+            @PathVariable @ApiParam(value = "Movie Id to delete movie object", required = true) Long id
+    ) {
         movieService.deleteMovie(id);
         return ResponseEntity.ok().build();
     }
-
     @GetMapping("/search")
-    @ApiOperation(value = "Search for movies in the system", response = List.class)
-    public ResponseEntity<List<Movie>> searchMovies(@RequestParam @ApiParam(value = "Phrase to search for movies", required = true) String phrase) {
-        return ResponseEntity.ok(movieService.searchMovies(phrase));
+    @ApiOperation(value = "Search for movies in the system", response = Page.class)
+    public ResponseEntity<Page<Movie>> searchMovies(
+            @RequestParam @ApiParam(value = "Phrase to search for movies", required = true) String phrase,
+            @RequestParam(defaultValue = "0") int page,
+            @RequestParam(defaultValue = "10") int size,
+            @RequestParam(defaultValue = "title") String sort,
+            @RequestParam(defaultValue = "asc") String direction
+    ) {
+        PageRequest pageRequest = PageRequest.of(
+                page, size,
+                direction.equalsIgnoreCase("asc") ? Sort.Direction.ASC : Sort.Direction.DESC,
+                sort
+        );
+        return ResponseEntity.ok(movieService.searchMovies(phrase, pageRequest));
     }
 
-    @PutMapping("/{id}/rating")
-    @ApiOperation(value = "Rate a movie", response = Movie.class)
-    public ResponseEntity<Movie> rateMovie(@PathVariable @ApiParam(value = "Movie Id to rate", required = true) Long id,
-                                           @RequestParam @ApiParam(value = "Rating of the movie", required = true) int rating) {
-        return ResponseEntity.ok(movieService.rateMovie(id, rating));
-    }
-
-    @GetMapping("/categories/{category}")
-    @ApiOperation(value = "Filter movies by category", response = List.class)
-    public ResponseEntity<List<Movie>> filterMoviesByCategory(@PathVariable @ApiParam(value = "Category of the movie", required = true) String category) {
-        return ResponseEntity.ok(movieService.filterMoviesByCategory(category));
-    }
 }
--- src/main/java/com/paszkowski/movies/repository/MovieRepository.java
+++ src/main/java/com/paszkowski/movies/repository/MovieRepository.java
@@ -1,18 +1,23 @@
 package com.paszkowski.movies.repository;
 
 import com.paszkowski.movies.model.Movie;
-import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
 
 @Repository
-public interface MovieRepository extends JpaRepository<Movie, Long> {
+public interface MovieRepository extends PagingAndSortingRepository<Movie, Long> {
+
+    Page<Movie> findAll(Pageable pageable);
 
     List<Movie> findByTitleContainingIgnoreCase(String phrase);
+    Page<Movie> findByTitleContainingIgnoreCase(String phrase, Pageable pageable);
+
     List<Movie> findByCategory(String category);
+    Page<Movie> findByCategory(String category, Pageable pageable);
     boolean existsByTitle(String title);
-
-
 }
